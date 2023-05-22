1ee16a8fefa7c240b926e4c909e22806081699ae
mantovani <g_mantovani@tutanota.com>
2023-05-21 22:37:17 -0300

    import find-config in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,16 @@
 const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config({ path: require('find-config')('.env') });
 
 // Conexão com o banco de dados MySQL
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-
-	host: 'localhost',
-	dialect: 'mysql'
-
-});
+const sequelize = new Sequelize(
+	process.env.MYSQL_DATABASE,
+	process.env.MYSQL_USER,
+	process.env.MYSQL_PASSWORD, {
+		host: '127.0.0.1',
+		port: 3306,
+		dialect: 'mysql'
+	}
+);
 
 module.exports = {
 
