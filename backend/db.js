a4cbdf2a2b0bd3a7ba673b863d27958d7f73fd1e
mantovani <g_mantovani@tutanota.com>
2023-08-31 19:01:41 -0300

    build: adding service for my node backend

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config({ path: require('find-config')('.env') });
 
-// Conexão com o banco de dados MySQL
-const sequelize = new Sequelize(
-	process.env.MYSQL_DATABASE,
-	process.env.MYSQL_USER,
-	process.env.MYSQL_PASSWORD, {
-		host: '127.0.0.1',
-		port: 3306,
-		dialect: 'mysql'
-	}
-);
+const sequelize = new Sequelize({
+    host: 'db', // Alteração aqui para o nome do serviço
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql',
+});
 
 module.exports = {
-
     Sequelize: Sequelize,
     sequelize: sequelize
-
 }
